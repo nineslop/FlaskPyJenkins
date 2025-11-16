027013eb27f7495f80d7c594793f19ca4ca3259d
nineslop <vladislav.ivanov.2004@bk.ru>
2025-11-16 18:40:26 +0300

    Jenkins/Flask integration Update

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_geek():
     return '<h1>Hello from Flask & Docker</h2>'
 
+@app.route('/health')
+def health_check():
+    return 'OK'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
